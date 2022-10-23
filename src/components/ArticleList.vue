168867ca350dbab4907c0392e7119d3f7c40b604
Uqyanzie <uqyanzie@gmail.com>
2022-10-23 12:11:09 +0700

    trial component

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -26,11 +26,36 @@
                     </div>
                 </div>
             </li>
+            <li>
+                <div class="card w-100">
+                    <div class="card-body d-flex justify-content-between">
+                        <img class="flex-item" src="../assets/image.svg" alt="">
+                        <div class="flex-item">
+                            <h6 class="card-title">
+                                <span>
+                                    <img src="../assets/creator.png" alt="">
+                                </span>
+                                Creator Name
+                                <span style="float : right">
+                                    <img src="../assets/save.svg" alt="">
+                                    <img src="../assets/report.svg" alt="">
+                                </span>
+                            </h6>
+                            <h4 class="card-subtitle mb-2">Article Title</h4>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam vero
+                                quos, saepe atque quae nostrum numquam sequi quis minus laboriosam, repellat, ipsum
+                                voluptatem laudantium perspiciatis facilis. Numquam voluptate quod nesciunt.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+            </li>
         </ul>
     </div>
 </template>
 
-<style>
+<style scoped>
 .article-list {
     display: flex;
     margin: 0;
@@ -40,6 +65,7 @@
     border-radius: 0;
     border: 0 0 1px 1px;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px 0px, rgba(0, 0, 0, 0.23) 0px 3px 6px 0px;
+    margin-top: 50px;
 }
 
 ul {
--- src/components/ArticlePage.vue
+++ src/components/ArticlePage.vue
@@ -1,47 +1,20 @@
 <template>
   <div class="article-page d-flex">
     <ArticleNavbar></ArticleNavbar>
-    <div class="col col-xl article">
-      <div class="container article-tab">
-        <nav>
-          <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <button class="nav-link active" id="nav-for-you-tab" data-bs-toggle="tab" data-bs-target="#nav-for-you"
-              type="button" role="tab" aria-controls="nav-for-you" aria-selected="true">For you</button>
-            <button class="nav-link" id="nav-following-tab" data-bs-toggle="tab" data-bs-target="#nav-following"
-              type="button" role="tab" aria-controls="nav-following" aria-selected="false">Following</button>
-            <button class="nav-link" id="nav-bookmark-tab" data-bs-toggle="tab" data-bs-target="#nav-bookmark"
-              type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Bookmarked</button>
-          </div>
-        </nav>
-        <div class="tab-content" id="nav-tabContent">
-          <div class="tab-pane fade show active" id="nav-for-you" role="tabpanel" aria-labelledby="nav-for-you-tab"
-            tabindex="0">
-            <h1>Home</h1>
-            <ArticleList></ArticleList>
-          </div>
-          <div class="tab-pane fade" id="nav-following" role="tabpanel" aria-labelledby="nav-following-tab"
-            tabindex="0">
-            <h1>Profile</h1>
-            <ArticleList></ArticleList>
-          </div>
-          <div class="tab-pane fade" id="nav-bookmark" role="tabpanel" aria-labelledby="nav-bookmark-tab" tabindex="0">
-            <h1>Contact</h1>
-            <ArticleList></ArticleList>
-          </div>
-        </div>
-      </div>
+    <div class="article col col-xl">
+      <ArticleTab></ArticleTab>
     </div>
   </div>
 </template>
 
 <script>
 import ArticleNavbar from './ArticleNavbar.vue'
-import ArticleList from './ArticleList.vue'
+import ArticleTab from './ArticleTab.vue';
 export default {
   name: 'App',
   components: {
     ArticleNavbar,
-    ArticleList
+    ArticleTab
   }
 }
 </script>
@@ -57,12 +30,5 @@ export default {
 .article {
   border-radius: 50px 0 0 0;
   background: #E5E5E5;
-
-}
-
-.article-tab {
-  position: relative;
-  left: 2%;
-  top: 3%;
 }
 </style>
